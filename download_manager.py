f79f63ceeaac061ed59c9bff4162754b26176d5b
Moofire <168238945+MoofireX@users.noreply.github.com>
2025-05-26 14:26:31 -0500

    Update download_manager.py
    added os detection. now compatible on all major platforms (unix like and windows)

--- download_manager.py
+++ download_manager.py
@@ -1,19 +1,28 @@
 import os
 import shutil
+import platform
+
+system = platform.system()
 
 categories = {"Videos" : ["mov", "mp4"], "Documents" : ["docx", "pdf", "md", "txt", "odt", "doc"], "Images" : ["jpg", "svg", "jpeg", "png", "gif"], "Installers" : ["iso", "dmg", "exe", "deb", "appimage"], "Zip Files" : ["zip", "gz", "tar", "7z", "rar"], "Unknown" : ["unknown"], "Code" : ["py", "cpp", "js", "java", "cs", "sh"],
               "Audio" : ["mp3", "wav"], "Spreadsheet" : ["csv", "xls", "xlsx", "ods"]}
 
 user = os.getlogin()
-os.chdir(f"/home/{user}/Downloads")
-dir = f"/home/{user}/Downloads"
+
+if system == "Linux" or system == "Darwin":
+    path = f"/home/{user}/Downloads"
+elif system == "Windows":
+    path = f"C:\\Users\\{user}\\Downloads"
+
+os.chdir(path)
+dir = path
 files = []
 file_paths = []
 type = []
 moved = False
 
 for file in os.listdir(dir):
-    file_path = os.path.join(dir, file).strip(f"/home/{user}/Downloads")
+    file_path = os.path.join(dir, file).strip(path)
     file_path2 = os.path.join(dir, file)
     if os.path.isfile(file_path2):
         files.append(file)
@@ -36,10 +45,10 @@ for file in files:
         for file_type in categories[val]:
             if file_list[-1] == file_type:
                 print(f"Moving {file} to {val}.")
-                shutil.move(f"/home/{user}/Downloads/{file}", f"/home/{user}/Downloads/{val}/{files[files.index(file)]}")
+                shutil.move(f"{path}/{file}", f"{path}/{val}/{files[files.index(file)]}")
                 moved = True
             
 if moved == False:
     print(f"Moving unknown file: {file} to directory: 'Unknown'.")
-    shutil.move(f"/home/{user}/Downloads/{file}", f"/home/{user}/Downloads/Unknown/{files[files.index(file)]}")
+    shutil.move(f"{path}/{file}", f"{path}/Unknown/{files[files.index(file)]}")
 
